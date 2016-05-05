ee318582084f1e475c450b07857834fae5f25528
Ronyerison <ronyerisonbraga@gmail.com>
2016-05-05 10:49:45 -0300

    Modelagem dos estados e transicoes.

--- src/main/java/br/ufpi/automatos/modelo/Estado.java
+++ src/main/java/br/ufpi/automatos/modelo/Estado.java
@@ -1,5 +1,90 @@
 package br.ufpi.automatos.modelo;
 
-public class Estado {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Estado<T> {
+	private Estado<T> estadoPai = null;
+	private T info = null;
+	private List<Estado<T>> estadosAdj = new ArrayList<Estado<T>>();
+	private List<Transicao<String, T>> transicoes = new ArrayList<Transicao<String,T>>();
+
+	public Estado(T info) {
+		this.info = info;
+	}
+
+	public Estado(T info, Estado<T> estadoPai, String valorTransicao) {
+		this.info = info;
+		this.estadoPai = estadoPai;
+		this.estadoPai.addTransicao(new Transicao<String, T>(valorTransicao, estadoPai, this));
+	}
+
+	public List<Estado<T>> getEstadosAdjacentes() {
+		return estadosAdj;
+	}
+
+	public void setEstadoPai(Estado<T> estadoPai, String valorTransicao) {
+		estadoPai.addTransicao(this, valorTransicao);
+		this.estadoPai = estadoPai;
+	}
+
+	public void addTransicao(T info, String valorTransicao) {
+		Estado<T> child = new Estado<T>(info);
+		this.estadosAdj.add(child);
+		this.addTransicao(new Transicao<String, T>(valorTransicao, this, child));
+	}
+
+	public void addTransicao(Estado<T> destino, String valorTransicao) {
+		this.estadosAdj.add(destino);
+		this.transicoes.add(new Transicao<String, T>(valorTransicao, this, destino));
+	}
+
+	public T getInfo() {
+		return this.info;
+	}
+
+	public void setInfo(T info) {
+		this.info = info;
+	}
+
+	public boolean isRoot() {
+		return (this.estadoPai == null);
+	}
+
+	public boolean isLeaf() {
+		if (this.estadosAdj.size() == 0)
+			return true;
+		else
+			return false;
+	}
+
+	public void removeEstadoPai() {
+		this.estadoPai = null;
+	}
+
+	/**
+	 * @return the transicoes
+	 */
+	public List<Transicao<String, T>> getTransicoes() {
+		return transicoes;
+	}
+
+	/**
+	 * @param transicoes the transicoes to set
+	 */
+	public void setTransicoes(List<Transicao<String, T>> transicoes) {
+		this.transicoes = transicoes;
+	}
+	
+	public void addTransicao(Transicao<String, T> transicao){
+		this.transicoes.add(transicao);
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return info+"";
+	}
 }
--- src/main/java/br/ufpi/automatos/modelo/Transicao.java
+++ src/main/java/br/ufpi/automatos/modelo/Transicao.java
@@ -1,5 +1,112 @@
 package br.ufpi.automatos.modelo;
 
-public class Transicao {
+public class Transicao<E, V> {
+	private E info;
+	private Estado<V> origem;
+	private Estado<V> destino;
+	
+	/**
+	 * @param info
+	 * @param origem
+	 * @param destino
+	 */
+	public Transicao(E info, Estado<V> origem, Estado<V> destino) {
+		this.info = info;
+		this.origem = origem;
+		this.destino = destino;
+	}
 
+	/**
+	 * @return the info
+	 */
+	public E getInfo() {
+		return info;
+	}
+
+	/**
+	 * @param info the info to set
+	 */
+	public void setInfo(E info) {
+		this.info = info;
+	}
+
+	/**
+	 * @return the origem
+	 */
+	public Estado<V> getOrigem() {
+		return origem;
+	}
+
+	/**
+	 * @param origem the origem to set
+	 */
+	public void setOrigem(Estado<V> origem) {
+		this.origem = origem;
+	}
+
+	/**
+	 * @return the destino
+	 */
+	public Estado<V> getDestino() {
+		return destino;
+	}
+
+	/**
+	 * @param destino the destino to set
+	 */
+	public void setDestino(Estado<V> destino) {
+		this.destino = destino;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return info + "";
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((destino == null) ? 0 : destino.hashCode());
+		result = prime * result + ((info == null) ? 0 : info.hashCode());
+		result = prime * result + ((origem == null) ? 0 : origem.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Transicao<E, V> other = (Transicao<E, V>) obj;
+		if (destino == null) {
+			if (other.destino != null)
+				return false;
+		} else if (!destino.equals(other.destino))
+			return false;
+		if (info == null) {
+			if (other.info != null)
+				return false;
+		} else if (!info.equals(other.info))
+			return false;
+		if (origem == null) {
+			if (other.origem != null)
+				return false;
+		} else if (!origem.equals(other.origem))
+			return false;
+		return true;
+	}
 }
