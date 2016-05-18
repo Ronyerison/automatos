ff61cbc1d9d80d0b05d505d43fd33d1faee9f8f9
Vanderson Moura <vanderson.moura.vm@gmail.com>
2016-05-18 16:13:29 -0300

    Implementados os métodos para obter um fecho de um estado dada uma transição.

--- src/main/java/br/ufpi/automatos/algoritmos/conversor/AFN2AFDConversor.java
+++ src/main/java/br/ufpi/automatos/algoritmos/conversor/AFN2AFDConversor.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import br.ufpi.automatos.modelo.Automato;
 import br.ufpi.automatos.modelo.Estado;
-import br.ufpi.automatos.modelo.InfoEstado;
 import br.ufpi.automatos.modelo.Transicao;
 
 public class AFN2AFDConversor {
@@ -14,66 +13,38 @@ public class AFN2AFDConversor {
 	
 	public AFN2AFDConversor() {
 	}
-//	
-//	@SuppressWarnings("unused")
-//	public Automato<InfoEstado, String> converter(Automato<InfoEstado, String> automatoAFN){
-//		Automato<InfoEstado, String> automatoAFD = new Automato<>();
-//		List<Estado<InfoEstado>> estadosAFD = new ArrayList<>();
-//		List<Estado<InfoEstado>> estados = automatoAFN.getEstados();
-//		List<Transicao<String, InfoEstado>> transicoesAFN = automatoAFN.getTransicoes();
-//		List<String> alfabeto = obterAlfabeto(automatoAFN.getTransicoes());
-//		
-//		FechoTransitivo ft;
-//		List<String> estadosAtingidos; 
-//		List<FechoTransitivo> fechos = new ArrayList<>();
-//		
-//		for (Estado<InfoEstado> estado : estados) {
-//			for (String label : alfabeto) {
-//				estadosAtingidos = new ArrayList<>();
-//				ft = new FechoTransitivo();
-//				ft.setLabelEstado(estado.getInfo().getLabel());
-//				ft.setLabelAlfabeto(label);
-//				for (Transicao<String, InfoEstado> transicao : transicoesAFN) {
-//					if(transicao.getInfo().equals(label)){
-//						if(estado.getInfo().getLabel().equals(transicao.getOrigem().getInfo().getLabel())){
-//							estadosAtingidos.add(transicao.getDestino().getInfo().getLabel());
-//						}
-//					}
-//				}
-//				ft.setEstadosAtingidos(estadosAtingidos);
-//				fechos.add(ft);
-//			}
-//		}
-//		return null; 
-//	}
-//	
-//	private List<String> obterAlfabeto (List<Transicao<String, InfoEstado>> transicoes){
-//		List<String> alfabeto = new ArrayList<>();
-//		
-//		for (Transicao<String, InfoEstado> t : transicoes) {
-//			String label = t.getInfo();
-//			if(!label.equals(ELEMENTO_VAZIO) && !existeLabel(alfabeto, label)){
-//				alfabeto.add(label);
-//			}
-//		} 
-//		return null;
-//	}
-//	
-//	private boolean existeLabel(List<String> lista, String label){
-//		for (String l : lista) {
-//			if(label.equals(l)){
-//				return true;
-//			}
-//		}
-//		return false;
-//	}	
 	
-	
-	@SuppressWarnings("unchecked")
 	public <E, T> Automato<E, T> converter(Automato<E, T> automatoAFN){
-		
 		return null;
+	}
+	
+	public <E, T> List<Estado<E>> construirFecho(Automato<E, T> automato, Estado<E> estado, T letra) {
+		List<Estado<E>> fecho = new ArrayList<>();
+		List<Estado<E>> f1 = new ArrayList<>();
+		f1.addAll(construirFechoVazio(automato, estado));
+		List<Estado<E>> f2 = new ArrayList<>(construirFechoPorLetra(f1, automato.getTransicoes(), letra));
+		List<Estado<E>> f3 = new ArrayList<>();
 		
+		for (Estado<E> e : f2) {
+			f3.addAll(construirFechoVazio(automato, e));
+		}
+		
+		fecho.addAll(f3);
+		return fecho;
+	}
+	
+	private <E, T, L> List<Estado<E>> construirFechoPorLetra(List<Estado<E>> listaEstados, List<Transicao<T, E>> listaTransicoes, L letra){
+		List<Estado<E>> estadosAux = new ArrayList<>();
+		for (Estado<E> e : listaEstados) {
+			for (Transicao<T, E> t : listaTransicoes) {
+				if(t.getOrigem().getInfo().equals(e.getInfo()) && t.getInfo().equals(letra)){
+					if(!estadosAux.contains(t.getDestino())){
+						estadosAux.add(t.getDestino());
+					}
+				}
+			}
+		}
+		return estadosAux;
 	}
 	
 	private <E, T> List<Estado<E>> construirFechoVazio(Automato<E, T> automato, Estado<E> estadoRef){
@@ -100,8 +71,8 @@ public class AFN2AFDConversor {
 		Estado<E> estadoReferencia = listaEstados.get(indice);
 		for (Transicao<T, E> t : listaTransicoes) {
 			if(t.getOrigem().getInfo().equals(estadoReferencia.getInfo()) && t.getInfo().equals(ELEMENTO_VAZIO)){
-				if(!listaEstados.contains(t.getOrigem())){
-					listaEstados.add(t.getOrigem());
+				if(!listaEstados.contains(t.getDestino())){
+					listaEstados.add(t.getDestino());
 				}
 			}
 		}
