70057e9c702d63562d708d7695463b2d95cf3325
Vanderson Moura <vanderson.moura.vm@gmail.com>
2016-05-25 21:52:24 -0300

    Finalizado o algoritmo de conversão de autômatos AFN em AFD.

--- src/main/java/br/ufpi/automatos/algoritmos/conversor/AFN2AFDConversor.java
+++ src/main/java/br/ufpi/automatos/algoritmos/conversor/AFN2AFDConversor.java
@@ -3,9 +3,6 @@ package br.ufpi.automatos.algoritmos.conversor;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.faces.view.facelets.FaceletException;
-import javax.persistence.metamodel.ListAttribute;
-
 import br.ufpi.automatos.modelo.Automato;
 import br.ufpi.automatos.modelo.Estado;
 import br.ufpi.automatos.modelo.InfoEstado;
@@ -18,16 +15,20 @@ public class AFN2AFDConversor<E, T> {
 	}
 	
 	public Automato<E, T> converter(Automato<E, T> automatoAFN){
-		List<FechoTransitivo<E, T>> fechos = new ArrayList<>();
+
+		List<FechoTransitivo<E, T>> fechos = new ArrayList<>();		
+		List<T> alfabeto = new ArrayList<>();
+
 		fechos = obterFechos(automatoAFN);
-//		List<T> alfabeto = obterAlfabeto(automatoAFN);
+		alfabeto = obterAlfabeto(automatoAFN);
+		criarEstadosCompostos(fechos, alfabeto);
 		
-		return null;
+		return criarAutomatoAFD(fechos);
 	}
 
 	/** MÉTODOS PARA OBTER A LISTA DE FECHOS TRANSITIVOS DO AUTOMATO**/
 	
-	public List<FechoTransitivo<E, T>> obterFechos(Automato<E, T> automato) {
+	private List<FechoTransitivo<E, T>> obterFechos(Automato<E, T> automato) {
 		List<T> alfa = new ArrayList<>();
 		alfa = obterAlfabeto(automato);
 		List<FechoTransitivo<E, T>> fechos = new ArrayList<>();
@@ -48,7 +49,7 @@ public class AFN2AFDConversor<E, T> {
 		return fechos;
 	}
 	
-	public List<Estado<E>> construirFecho(Automato<E, T> automato, Estado<E> estado, T letra) {
+	private List<Estado<E>> construirFecho(Automato<E, T> automato, Estado<E> estado, T letra) {
 		List<Estado<E>> fecho = new ArrayList<>();
 		List<Estado<E>> f1 = new ArrayList<>();
 		f1.addAll(construirFechoVazio(automato, estado));
@@ -108,7 +109,7 @@ public class AFN2AFDConversor<E, T> {
 		}
 	}
 	
-	public List<T> obterAlfabeto(Automato<E, T> automato){
+	private List<T> obterAlfabeto(Automato<E, T> automato){
 		List<T> alfabeto = new ArrayList<>();
 		for (Transicao<T, E> t : automato.getTransicoes()) {
 			if(!alfabeto.contains(t.getInfo()) && !t.getInfo().equals(Constante.getElementoVazio())){
@@ -120,7 +121,7 @@ public class AFN2AFDConversor<E, T> {
 	
 	/** MÉTODOS PARA ADICIONAR OS POSSÍVEIS NOVOS ESTADOS À LISTA DE FECHOS DO AUTOMATO **/
 	
-	public void criarEstadosCompostos(List<FechoTransitivo<E, T>> fechos, List<T> alfabeto){
+	private void criarEstadosCompostos(List<FechoTransitivo<E, T>> fechos, List<T> alfabeto){
 		List<List<Estado<E>>> listaFechosCompostos = new ArrayList<>();
 		List<List<Estado<E>>> listaFechosCompostosAux = new ArrayList<>();
 		int tamFecho = 0;
@@ -207,22 +208,137 @@ public class AFN2AFDConversor<E, T> {
 	
 	/** MÉTODOS PARA CRIAR O AUTOMATO AFD A PARTIR DA LISTA FINAL DE FECHOS TRANSITIVOS **/
 	
-	public Automato<E, T> criarAutomatoAFD(List<FechoTransitivo<E, T>> fechos){
-		List<Estado<E>> estados = new ArrayList<>();
-		List<Transicao<T, E>> transicoes = new ArrayList<>();
-		List<FechoTransitivo<E, T>> fechosAux = new ArrayList<>(fechos);
+	@SuppressWarnings("unchecked")
+	private Automato<E, T> criarAutomatoAFD(List<FechoTransitivo<E, T>> fechos){
+		Automato<E, T> automatoAFD = new Automato<>();
+		List<List<Estado<E>>> estadosAtingidos = new ArrayList<>();
+		List<Estado<E>> novosEstados = new ArrayList<>();
+		List<Transicao<T, E>> novasTransicoes = new ArrayList<>();
 		
-		for (FechoTransitivo<E, T> f1 : fechos) {
-			for (FechoTransitivo<E, T> f2 : fechosAux) {
-				if(compararFechos(f1.getEstadoUnificado(), f2.getFecho())){
-					
+		Estado<E> novoEstado;
+		Transicao<T, E> novaTransicao;
+		
+		for (FechoTransitivo<E, T> f : fechos) {
+			if(f.getFecho().size() > 0){
+				if(!existeNaListaFechosUnicos(f.getFecho(), estadosAtingidos)){
+					estadosAtingidos.add(new ArrayList<>(f.getFecho()));
 				}
 			}
 		}
 		
+		for (FechoTransitivo<E, T> f : fechos) 	{
+			for (List<Estado<E>> e : estadosAtingidos) {
+				if(compararFechos(f.getEstadoUnificado(), e)){
+					InfoEstado info = new InfoEstado(f.infoEstadoUnificado());
+					novoEstado = (Estado<E>) new Estado<InfoEstado>(info);
+					if(verificarEstadoMarcado(e)){
+						novoEstado.setMarcado(true);
+					}
+					if(verificarEstadoInicial(e)){
+						novoEstado.setInicial(true);
+					}
+					if(!existeEstadoLista(novosEstados, novoEstado)){
+						novosEstados.add(novoEstado);
+					}
+				}
+			}
+		}
 		
- 		return null;
+		List<Estado<E>> l1, l2;
+		
+		for (FechoTransitivo<E, T> f : fechos) {
+			l1 = buscarEstados(f.getEstadoUnificado(), estadosAtingidos);
+			l2 = buscarEstados(f.getFecho(), estadosAtingidos);
+			if(l1 != null && l2 != null){
+				Estado<E> e1 = buscarNovoEstadoPorEstadoUnif(l1, novosEstados);
+				Estado<E> e2 = buscarNovoEstadoPorEstadoUnif(l2, novosEstados);
+				novaTransicao = new Transicao<T, E>(f.getInfoTransicao(), e1, e2);
+				novasTransicoes.add(novaTransicao);
+			}
+		}
+		
+		automatoAFD.setEstados(novosEstados);
+		automatoAFD.setTransicoes(novasTransicoes);
+		discriminarInicialMarcado(automatoAFD);
+ 		return automatoAFD;
 	}
 	
+	private boolean existeNaListaFechosUnicos(List<Estado<E>> fecho, List<List<Estado<E>>> fechosUnicos){
+		for (List<Estado<E>> f : fechosUnicos) {
+			if(compararFechos(fecho, f)){
+				return true;
+			}
+		}
+		return false;
+	}
 	
+	private boolean verificarEstadoMarcado(List<Estado<E>> estados){
+		for (Estado<E> estado : estados) {
+			if(estado.isMarcado()){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private boolean verificarEstadoInicial(List<Estado<E>> estados){
+		if(estados.size() == 1){
+			if(estados.get(0).isInicial()){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private List<Estado<E>> buscarEstados(List<Estado<E>> estadoUnificado, List<List<Estado<E>>> listaEstadosUnificados){
+		for (List<Estado<E>> l : listaEstadosUnificados) {
+			if(compararFechos(l, estadoUnificado)){
+				return l;
+			}
+		}
+		return null;
+	}
+	
+	@SuppressWarnings("unchecked")
+	private Estado<E> buscarNovoEstadoPorEstadoUnif(List<Estado<E>> estadoUnif, List<Estado<E>> novosEstados){
+		String[] strs;
+		List<Estado<E>> estadosAux;
+		
+		for (Estado<E> estado : novosEstados) {
+			strs =((String)(((InfoEstado)estado.getInfo()).getLabel())).split(",");
+			estadosAux = new ArrayList<>();
+			
+			for (String string : strs) {
+				estadosAux.add(new Estado<E>((E)new InfoEstado(string)));
+			}
+			if(compararFechos(estadosAux, estadoUnif)){
+				return estado;
+			}
+		}
+		return null;
+	}
+	
+	private boolean existeEstadoLista(List<Estado<E>> estados, Estado<E> estado){
+		for (Estado<E> e : estados) {
+			if(((InfoEstado)e.getInfo()).getLabel().equals(((InfoEstado)estado.getInfo()).getLabel())){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private void discriminarInicialMarcado(Automato<E, T> automato){
+		Estado<E> estadoInicial = null;
+		List<Estado<E>> estadosMarcados = new ArrayList<>();
+		for (Estado<E> e : automato.getEstados()) {
+			if(e.isInicial()){
+				estadoInicial = e;
+			}
+			if(e.isMarcado()){
+				estadosMarcados.add(e);
+			}
+		}
+		automato.setEstadoInicial(estadoInicial);
+		automato.setEstadosMarcados(estadosMarcados);
+	}
 }
